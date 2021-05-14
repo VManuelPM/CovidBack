aa04ca8c661c7cf22553d361df38d14c9b8b2a19
vmanuelpm <vmanuelpm97@hotmail.com>
2021-05-14 22:28:26 +0200

    added swagger

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 //Import Routes
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
+//Import Swagger
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 dotenv.config();
 
@@ -13,11 +16,33 @@ mongoose.connect(process.env.DB_CONNECT, () => {
   console.log('connected to db!');
 });
 
+//Swagger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Covid API',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsDoc(swaggerOptions);
+
 //Middlewares
 app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 //Route Middelwares
 app.use('/api/user', authRoute);
 app.use('/api/posts', postRoute);
 
+//Listen port
 app.listen(3000, () => console.log('Server up and running'));
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,63 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { loginValidation, registerValidation } = require('../validation');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the user
+ *         email:
+ *           type: string
+ *           description: The email of the user
+ *         password:
+ *           type: string
+ *           description: The password of the user
+ *       example:
+ *         name: victor manuel
+ *         email: vpm570@inlumine.ual.es
+ *         password: SomePassword
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Users
+ *  description: Operations related with users
+ */
+
+/**
+ * @swagger
+ * /api/user/register:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The user was created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *          description: Email already exists, bad user formed or error
+ *       500:
+ *         description: Some server error
+ */
 router.post('/register', async (req, res) => {
   //VALIDATE DATA BEFORE WE A USER
   const { error } = registerValidation(req.body);
@@ -33,7 +90,26 @@ router.post('/register', async (req, res) => {
   }
 });
 
-//Login
+/**
+ * @swagger
+ * /api/user/login:
+ *   post:
+ *     summary: Login with username and password
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: Token  of JWT
+ *       400:
+ *          description: Email or password is wrong
+ *       500:
+ *         description: Some server error
+ */
 router.post('/login', async (req, res) => {
   //VALIDATE DATA BEFORE WE A USER
   const { error } = loginValidation(req.body);
@@ -45,10 +121,10 @@ router.post('/login', async (req, res) => {
   if (!user) return res.status(400).send('Email or password is wrong');
   //Password is correct
   const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send('Invalid password');
+  if (!validPass) return res.status(400).send('Email or password is wrong');
   //Create and assign a token
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-  res.header('auth-token', token).send(token);
+  res.status(200).header('auth-token', token).send(token);
 });
 
 module.exports = router;
